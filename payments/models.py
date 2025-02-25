3433386a3cbe9cc2d9ae548acb364f985ce9ad3b
Egor <khutornoy_ea@devtrix.ru>
2025-02-25 17:21:04 +0700

    Initial commit: Django project with Stripe integration

---
+++ payments/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Item(models.Model):
+    """Модель товара"""
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
